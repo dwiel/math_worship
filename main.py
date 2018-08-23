7c13cdca86d967a1cda5842a21ab9bdbaa674828
Nate O'Donnell <nodonnel@gmail.com>
2018-08-23 09:23:25 -0400

    disabled block_start and block_size features due to buggy operation. Automated playing of Player objects each cycle. Continued to experiment with synth and drum sequences using 'x' as rests.

--- main.py
+++ main.py
@@ -35,35 +35,45 @@ class Sequencer():
             self.block_size = block_size
 
         self.block_start = block_start
-        self.out = self.block()[0]
+        #self.out = self.block()[0]
+        self.out = base_sequence[0]
         self.last_out = "init"
         self.last_time = 0
         self.play_me = True
         print(base_sequence)
 
+
+
     def block(self):
         block_start = min(self.block_start % len(self.base_sequence), (self.block_size + self.block_start) % len(self.base_sequence))
-        return self.base_sequence[block_start:(self.block_start + self.block_size)]
+        block_stop = max(block_start, ((self.block_size + self.block_start + 1) % len(self.base_sequence)))
+        return self.base_sequence[block_start:block_stop]
+
 
     def update(self):
 
         self.last_out = self.out
         self.last_pos = self.pos
 
-        block = self.block()
+        #block = self.block()
 
         cycles_per_step = float(cycles_per_beat) / self.tempo_mult
         self.pos += (1 / cycles_per_step)
-        self.pos = self.pos % len(block)
-        self.out = block[int(self.pos)]
+        #self.pos = self.pos % len(block)
+        #self.out = block[int(self.pos)]
+        self.pos = self.pos % len(self.base_sequence)
+        self.out = self.base_sequence[int(self.pos)]
 
         if self.last_out != self.out:
-            self.pos = (self.pos + self.increment - 1) % len(block)
-            self.out = block[int(self.pos)]
+            #self.pos = (self.pos + self.increment - 1) % len(block)
+            #self.out = block[int(self.pos)]
+            self.pos = (self.pos + self.increment - 1) % len(self.base_sequence)
+            self.out = self.base_sequence[int(self.pos)]
 
             print(self.name, self.out)
 
         return self.out
+
 
 class Player():
     def __init__(self, name, octave=0, channel=1):
@@ -77,8 +87,8 @@ class Player():
 
     def play(self, sequencer):
 
-        print(sequencer.pos)
-        print(sequencer.out)
+        #print(sequencer.pos)
+        #print(sequencer.out)
 
         if isinstance(sequencer.out, numbers.Number):
             self.current_note = int(degree_to_mode(sequencer.out, self.octave)) # should be MIDI value
@@ -119,43 +129,6 @@ class Player():
 
             return None
 
-"""
-class Player():
-    def __init__(self, name, octave=0):
-        self.name = name
-        self.octave = octave
-        self.last_note = "init"
-        self.current_note = "init"
-        self.play_me = True
-        self.init = True
-
-    def play(self, sequencer):
-
-        self.last_note = int(degree_to_mode(sequencer.last_out, self.octave)) # should be MIDI value
-        self.current_note = int(degree_to_mode(sequencer.out, self.octave)) # should be MIDI value
-
-        if self.last_note == self.current_note:
-            self.play_me = False
-        else:
-            self.play_me  = True
-
-        if self.play_me == True or self.init == True:
-
-            self.init = False
-
-            midiout.send_message([0x80, self.last_note, 0]) # note off message for last note played
-            midiout.send_message([0x90, self.current_note, 110]) # note on, channel 1, frequency of note, velocity
-
-            # TO DO: modify above code to divide amplitude by # of (non-muted/paused) Player objects
-
-            print(self.name, self.current_note)
-            return self.current_note
-
-        else:
-
-            return None
-
-"""
 
 class Drummer():
     def __init__(self, name, octave=0, channel=1):
@@ -169,8 +142,8 @@ class Drummer():
 
     def play(self, sequencer):
 
-        print(sequencer.pos)
-        print(sequencer.out)
+        #print(sequencer.pos)
+        #print(sequencer.out)
 
         if isinstance(sequencer.out, numbers.Number):
             self.current_note = int(36 + sequencer.out) # should be MIDI value
@@ -238,16 +211,23 @@ def degree_to_mode(degree, octave=0, key=global_key, mode=global_mode):
 
 sequencer_list = [] # initializing the list where all sequencers will be stored
 
-sequencer_list.append(Sequencer("synth0", [1,2,3,4,5], 1, 1))
-sequencer_list.append(Sequencer("synth1", [2,3,4], 1, 0.25))
-sequencer_list.append(Sequencer("synth2", [1,2,1,2,3,4,5], 1, 0.5))
-sequencer_list.append(Sequencer("drum0", [0,6,0,0,3,0,6,6], 1, 4)) # Would be nice to enter a string and have it convert to a list.
+sequencer_list.append(Sequencer("synth0", [0,1,2,3,"x",3,2,1], 1, 2))
+sequencer_list.append(Sequencer("synth1", [0,1,"x",3], 1, 1))
+sequencer_list.append(Sequencer("synth2", [0,2,0,1,2,3,"x",3,4,3,2,1], 1, 4))
+sequencer_list.append(Sequencer("synth3", [0,2,0,1,2,3,"x",3,4,3,2,1], 1, 3))
+
+sequencer_list.append(Sequencer("drum0", [0,"x",6,"x",0,"x",6,"x"], 1, 4)) # Would be nice to enter a string and have it convert to a list.
+sequencer_list.append(Sequencer("invisible1", [0.5, 1, 2], 1, 0.125))
+sequencer_list.append(Sequencer("invisible2", [0.5, 1, 2, 3], 1, 0.125))
+
 
 player_list = [] #inistalizes list where all players are stored
 
+player_list.append(Player("player0", 0))
 player_list.append(Player("player1", -1))
-player_list.append(Player("player2", -2))
-player_list.append(Player("player3", 0))
+player_list.append(Player("player2", 1))
+player_list.append(Player("player3", 2))
+
 
 drummer0 = Drummer("drum0_player")
 
@@ -255,27 +235,32 @@ drummer0 = Drummer("drum0_player")
 
 while t < 1000 / seconds_per_cycle:
 
-    # We need a "map" function that maps inputs to ouputs iff they are numeric
-
-    sequencer_list[0].tempo_mult = sequencer_list[1].out
-
-    sequencer_list[1].tempo_mult = sequencer_list[0].out/50
-    sequencer_list[1].increment = sequencer_list[0].out
-
-    sequencer_list[2].tempo_mult = sequencer_list[1].out
-    sequencer_list[2].block_size = sequencer_list[1].out + 1
-    sequencer_list[2].block_start = sequencer_list[1].out - 1
-    sequencer_list[2].increment = sequencer_list[1].out * sequencer_list[0].out
-
-    sequencer_list[3].increment = sequencer_list[0].out
-
-
-    # FIX THIS lol: global_key = 60 + sequencer_list[1].out
 
     for i in range(len(sequencer_list)):
         sequencer_list[i].update()
 
-    global_key = 60 + int(sequencer_list[1].out)
+        # This is where the mappings/connections between sequences are defined
+        # We need a "map" function that maps inputs to ouputs iff they are numeric
+
+        if isinstance(sequencer_list[1].out, numbers.Number):
+            sequencer_list[0].increment = sequencer_list[1].out + 2
+
+        #sequencer_list[1].tempo_mult = sequencer_list[4].out
+        if isinstance(sequencer_list[0].out, numbers.Number):
+            sequencer_list[1].increment = sequencer_list[0].out + 3
+
+        #sequencer_list[2].tempo_mult = sequencer_list[4].out
+        #sequencer_list[2].block_size = sequencer_list[1].out + 1
+        #sequencer_list[2].block_start = sequencer_list[1].out - 1
+        if isinstance(sequencer_list[0].out, numbers.Number):
+            sequencer_list[2].increment = sequencer_list[0].out + 4
+
+        if isinstance(sequencer_list[0].out, numbers.Number):
+            sequencer_list[3].increment = sequencer_list[0].out + sequencer_list[5].out
+
+        sequencer_list[3].tempo_mult = sequencer_list[5].out + sequencer_list[6].out
+
+    #global_key = 60 + int(sequencer_list[1].out)
 
     if global_key_temp != global_key and global_key_temp != "init":
         for i in range(len(player_list)):
@@ -284,11 +269,12 @@ while t < 1000 / seconds_per_cycle:
 
     global_key_temp = global_key
 
-    player_list[0].play(sequencer_list[0])
-    player_list[1].play(sequencer_list[1])
-    player_list[2].play(sequencer_list[2])
+    for i in range(len(player_list)):
+        player_list[i].play(sequencer_list[i])
 
-    drummer0.play(sequencer_list[3])
+
+
+    drummer0.play(sequencer_list[4])
 
 
     t = t + 1
